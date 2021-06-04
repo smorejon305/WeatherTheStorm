537ec5d94709c639cd3de96896eca7a5aa52201a
Stefanie Morejon <youvebeensmmored@gmail.com>
2021-06-04 16:43:28 +0200

    dropped files into the correct repository
    troubleshooting success following a 404 error on launch

---
+++ src/app.js
@@ -0,0 +1,90 @@
+// apiKey = b44c44391c0349160ac683c7b746d72d
+//apiUrl = https://api.openweathermap.org/data/2.5/weather?q={Paris}&units=metric&appid=b44c44391c0349160ac683c7b746d72d
+
+//WEEK 5 HOMEWORK
+
+function displayWeather(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+}
+
+function searchLocation(city) {
+  let apiKey = "b44c44391c0349160ac683c7b746d72d";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey} `;
+  axios.get(apiUrl).then(displayWeather);
+}
+
+//Search Behaviour
+
+function searchCity(city) {
+  let apiKey = "b44c44391c0349160ac683c7b746d72d";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  searchCity(city);
+}
+
+//current location button
+function searchLocation(position) {
+  let apiKey = "b44c44391c0349160ac683c7b746d72d";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayWeather);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let dateElement = document.querySelector("#date");
+let currentTime = new Date();
+dateElement.innerHTML = formatDate(currentTime);
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSubmit);
+
+let currentLocationButton = document.querySelector("#current-location");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
+//Formatted Date
+function formatDate(date) {
+  let hours = currentTime.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = currentTime.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let dayIndex = currentTime.getDay();
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[dayIndex];
+
+  return `${days[dayIndex]}  ${hours}: ${minutes}`;
+}
+
+searchCity("New York");
